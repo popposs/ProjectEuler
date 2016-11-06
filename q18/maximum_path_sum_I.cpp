75280d5892780da21473d42372abf8f576990143
josh.chang.28@gmail.com <josh.chang.28@gmail.com>
2016-11-05 20:47:23 -0500

    q18

--- q18/maximum_path_sum_I.cpp
+++ q18/maximum_path_sum_I.cpp
@@ -5,18 +5,16 @@
 using namespace std;
 
 int get(int level, int elem, vector<int> & v){
-	//for debug
 	if(level >= v.size())
 		return -1;
-	return v[level] + elem + 1;
+	return v[level] + elem;
 }
 
 int main(){
+
 	vector<string> vs;
 	vector<int> vi;
 	string readin = "";
-
-	vi.push_back(0);
 
 	ifstream wordsFile("triangle.txt");
 	string words;
@@ -26,22 +24,20 @@ int main(){
 		}
 	}
 
-string delimiter = " ";
-size_t pos = 0;
-string token;
-while ((pos = readin.find(delimiter)) != string::npos) {
-    token = readin.substr(0, pos);
-	vs.push_back(token);
-    readin.erase(0, pos + delimiter.length());
-}
+	string delimiter = " ";
+	size_t pos = 0;
+	string token;
+	while ((pos = readin.find(delimiter)) != string::npos) {
+   	 token = readin.substr(0, pos);
+		vs.push_back(token);
+   		readin.erase(0, pos + delimiter.length());
+	}
 
-for(int i = 0; i < 15; i++)
-	vi.push_back(i);
 
-cout << "(0, 0) " << vs[get(0,0, vi)] << endl;
-cout << "(1, 1) " << vs[get(1,1, vi)] << endl;
-cout << "(2, 3) " << vs[get(2,3, vi)] << endl;
-cout << "(3, 5) " << vs[get(3,5, vi)] << endl;
-cout << "(4, 4) " << vs[get(4,4, vi)] << endl;
+	vi.push_back(0);
+	for(int i = 1; i < 15; i++)
+		vi.push_back(i + vi[i-1]);
+
+	
 
 return 0;}
